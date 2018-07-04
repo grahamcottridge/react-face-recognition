efd027df3e91690065d1fd68ea795e7251caa8da
Graham Cottridge <codinggc@gmail.com>
2018-07-04 20:03:29 +0100

    Add face-recognition component and api functionality to recieve and display images

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import './App.css';
 
 const API_KEY =`${process.env.REACT_APP_API_KEY}`
@@ -30,25 +31,26 @@ class App extends Component {
     super();
     this.state = {
       input: '',
+      imageUrl: ''
     }
   }
 
 onInputChange = (event) => {
-  console.log(event.target.value);
+  this.setState({input: event.target.value});
 }
 
 onButtonSubmit = () => {
-  console.log('click');
-  app.models.predict(
-    "a403429f2ddf4b49b307e318f00e528b",
-    "https://samples.clarifai.com/face-det.jpg")
-  .then(
-    function(response) {
-      console.log(response);
-      // do something with response
-    },
-    function(err) {
-      // there was an error
+  this.setState({imageUrl: this.state.input});
+  app.models
+    .predict(
+      Clarifai.FACE_DETECT_MODEL,
+      this.state.input)
+    .then(
+      function(response) {
+        console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
+      },
+      function(err) {
+        // there was an error
     }
   );
 }
@@ -66,8 +68,7 @@ onButtonSubmit = () => {
             onInputChange={this.onInputChange}
             onButtonSubmit={this.onButtonSubmit}
           />
-
-          {/*<FaceRecognition /> */}
+          <FaceRecognition imageUrl={this.state.imageUrl} />
       </div>
     );
   }
