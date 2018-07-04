640e86a85dc59c2b31c06921f232e58e97778cd2
Graham Cottridge <codinggc@gmail.com>
2018-07-04 19:22:34 +0100

    Set up API key

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
 import Particles from 'react-particles-js';
-import Navigation from './components/Navigation/Navigation'
-import Logo from './components/Logo/Logo'
-import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
-import Rank from './components/Rank/Rank'
+import Clarifai from 'clarifai';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
 import './App.css';
+
+const API_KEY =`${process.env.REACT_APP_API_KEY}`
+
+const app = new Clarifai.App({
+ apiKey: API_KEY
+});
 
 const particlesOptions = {
   particles: {
@@ -32,6 +39,18 @@ onInputChange = (event) => {
 
 onButtonSubmit = () => {
   console.log('click');
+  app.models.predict(
+    "a403429f2ddf4b49b307e318f00e528b",
+    "https://samples.clarifai.com/face-det.jpg")
+  .then(
+    function(response) {
+      console.log(response);
+      // do something with response
+    },
+    function(err) {
+      // there was an error
+    }
+  );
 }
 
   render() {
