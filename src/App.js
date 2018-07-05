df34af29d3143867776febe51476a0b4e1d8c7fb
Graham Cottridge <codinggc@gmail.com>
2018-07-05 20:30:28 +0100

    Complete routing and minor refacting in app

--- src/App.js
+++ src/App.js
@@ -35,7 +35,8 @@ class App extends Component {
       input: '',
       imageUrl: '',
       box: {},
-      route: 'signin'
+      route: 'signin',
+      isSignedIn: false
     }
   }
 
@@ -54,7 +55,6 @@ calculateFaceLocation = (data) => {
 }
 
 displayFaceBox = (box) => {
-  console.log(box);
   this.setState({box: box})
 }
 
@@ -73,17 +73,23 @@ onButtonSubmit = () => {
 }
 
 onRouteChange = (route) => {
+  if (route === 'signout') {
+    this.setState({isSignedIn: false})
+  } else if (route === 'home') {
+    this.setState({isSignedIn: true})
+  }
   this.setState({route: route});
 }
 
   render() {
+    const { isSignedIn, imageUrl, route, box } = this.state
     return (
       <div className="App">
         <Particles className='particles'
             params={particlesOptions}
           />
-          <Navigation onRouteChange={this.onRouteChange} />
-          { this.state.route === 'home'
+          <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange} />
+          { route === 'home'
             ? <div>
                 <Logo />
                 <Rank />
@@ -91,10 +97,10 @@ onRouteChange = (route) => {
                   onInputChange={this.onInputChange}
                   onButtonSubmit={this.onButtonSubmit}
                 />
-                <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl} />
+                <FaceRecognition box={box} imageUrl={imageUrl} />
               </div>
             : (
-                this.state.route === 'signin'
+                route === 'signin'
                 ? <SignIn onRouteChange={this.onRouteChange} />
                 : <Register onRouteChange={this.onRouteChange} />
               )
