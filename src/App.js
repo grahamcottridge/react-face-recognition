99af255dfb95560eb8d8143957bfabe47617f129
Graham Cottridge <codinggc@gmail.com>
2018-07-17 02:00:52 +0100

    Implement register as stateful smart component and update rank for users

--- src/App.js
+++ src/App.js
@@ -36,7 +36,15 @@ class App extends Component {
       imageUrl: '',
       box: {},
       route: 'signin',
-      isSignedIn: false
+      isSignedIn: false,
+      user: {
+        id: '',
+        name: '',
+        email: '',
+        password: '',
+        entries: 0,
+        joined: ''
+      }
     }
   }
 
@@ -45,6 +53,17 @@ class App extends Component {
 //     .then(response => response.json())
 //     .then(console.log)
 // }
+
+loadUser = (data) => {
+  this.setState({user: {
+    id: data.id,
+    name: data.name,
+    email: data.email,
+    password: data.password,
+    entries: data.entries,
+    joined: data.joined
+  }})
+}
 
 calculateFaceLocation = (data) => {
   const clarifyFace = data.outputs[0].data.regions[0].region_info.bounding_box;
@@ -98,7 +117,7 @@ onRouteChange = (route) => {
           { route === 'home'
             ? <div>
                 <Logo />
-                <Rank />
+                <Rank name={this.state.user.name} entries={this.state.user.entries}/>
                 <ImageLinkForm
                   onInputChange={this.onInputChange}
                   onButtonSubmit={this.onButtonSubmit}
@@ -107,8 +126,8 @@ onRouteChange = (route) => {
               </div>
             : (
                 route === 'signin'
-                ? <SignIn onRouteChange={this.onRouteChange} />
-                : <Register onRouteChange={this.onRouteChange} />
+                ? <SignIn loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
+                : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
               )
           }
       </div>
--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -1,13 +1,11 @@
 import React from 'react';
 
-const Rank = () => {
+const Rank = ({name, entries}) => {
   return (
     <div>
-      <div className='white f3'>
-        {`Bob, your current entry count is...`}
-      </div>
-      <div className='white f1'>
-        {`#5`}
+      {`${name}, your current rank is...`}
+      <div className='white f1 '>
+        {entries}
       </div>
     </div>
   );
