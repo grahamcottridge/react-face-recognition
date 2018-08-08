986b027e31b8e50cb99f736f4c996d199d540131
Graham Cottridge <codinggc@gmail.com>
2018-08-08 13:48:49 +0100

    Update frontend layout

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -7,7 +7,7 @@ import './logo.css';
 
 const Logo = () => {
   return (
-    <div className='ma4 mt0'>
+    <div className='ma4 mt0 center'>
       <Tilt className="Tilt br2 shadow-2" options={{ max : 55 }} style={{ height: 100, width: 100 }} >
          <div className="Tilt-inner">
            <img style={{paddingTop: '9px'}} alt='logo' src={brain}/>
